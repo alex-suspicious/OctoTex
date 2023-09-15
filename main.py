b955d423c166f0d7f2a48c8e604ba2ab6f5a7a4b
alex <savav.cool@gmail.com>
2023-09-15 17:36:43 +0600

    GUI was added, fixed some issues

--- main.py
+++ main.py
@@ -1,16 +1,121 @@
 import time
 import tkinter
-from tkinter import ttk, PhotoImage
+from tkinter import ttk, PhotoImage, Menu, messagebox
 from OpenGL import GL
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import glfw
 import pygame
+import pathlib
 
 from pyopengltk import OpenGLFrame
 from tkinter.messagebox import showinfo
 import os
 
+import load as loader
+import upscale as upscaler
+import pbr as materializer
+import write as writer
+import config
+
+from zipfile import ZipFile
+
+mouse_pressed = False
+mouse_coord = [0,0]
+mouse_prev_coord = [0,0]
+
+pitch = 0
+yaw = 0
+
+temp_pitch = 0
+temp_yaw = 0
+
+def loadTexturesTkinter():
+	loadDirId = 0
+	loadDir = "captures"
+
+	mods = [f for f in pathlib.Path().glob(f"{config.rtx_remix_dir}/mods/*")]
+	if( len(mods) > 0 ):
+		print("Directories for importing: ")
+
+		print("0 | captures")
+		for x in range(len(mods)):
+			mod_path = str(mods[x]).replace(f"{config.rtx_remix_dir}/mods/","")
+			print(f"{x+1} | {mod_path}")
+
+
+		#loadDirId = int(input("From what dir you want to load the textures, select by index: "))
+		loadDirId = 0
+		loadDirId -= 1
+		if( loadDirId != -1 ):
+			loadDir = str(mods[loadDirId]).replace(f"{config.rtx_remix_dir}/","") + "/SubUSDs/textures/diffuse"
+		else:
+			loadDir += "/textures"
+	else:
+		loadDir += "/textures"
+
+	def endLoading( files ):
+		messagebox.showinfo("Loader", f"Loading is done!\n{files} textures was loaded.")
+		updateTextureTree()
+
+	loader.loadTextures(loadDir,0,0,endLoading)
+
+def upscaleTexturesTkinter():
+	messagebox.showinfo("Upscaler", f"Upscaling may take some time.\nPlease check the console for the upscaling status.\nPress OK if you understand everything.")
+	upscaler.upscaleTextures()
+
+	messagebox.showinfo("Upscaler", f"Upscaling is done!")
+	updateTextureTree()
+
+def upscale2xTexturesTkinter():
+	messagebox.showinfo("Upscaler 2x", f"Upscaling may take some time.\nPlease check the console for the upscaling status.\nPress OK if you understand everything.")
+	upscaler.upscaleTextures2X()
+
+	messagebox.showinfo("Upscaler 2x", f"Upscaling is done!")
+	updateTextureTree()
+
+def materializeTexturesTkinter():
+	messagebox.showinfo("Materializer", f"PBR generation may take some time.\nPlease check the console for the generation status.\nPress OK if you understand everything.")
+	materializer.generatePBR()
+
+	messagebox.showinfo("Materializer", f"PBR generation is done!")
+	updateTextureTree()
+
+def writeTexturesBack():
+	mod_dir = "OctoTexGUI"
+	isExist = os.path.exists(f"{config.rtx_remix_dir}/mods/{mod_dir}")
+	if not isExist:
+		with ZipFile("mods/modTemplate.zip", 'r') as zObject:
+			zObject.extractall( path=f"{config.rtx_remix_dir}/mods/")
+
+		os.rename(f"{config.rtx_remix_dir}/mods/modTemplate", f"{config.rtx_remix_dir}/mods/{mod_dir}")
+
+	replacements_file = "replacements.usda"
+	writer.saveAllTextures(mod_dir, replacements_file)
+	messagebox.showinfo("Writer", f"All textures has been written back!")
+
+def OnMouseDown(event):
+	global mouse_pressed
+	mouse_pressed = True
+
+def OnMouseUp(event):
+	global mouse_pressed
+	mouse_pressed = False
+	
+def motion(event):
+	global pitch, temp_pitch, yaw, temp_yaw, mouse_pressed, mouse_prev_coord, mouse_coord
+	x, y = event.x, event.y
+	
+	mouse_coord = [x,y]
+	if( not mouse_pressed ):
+		mouse_prev_coord = [x,y]
+		temp_pitch = pitch
+		temp_yaw = yaw
+	else:
+		yaw = temp_yaw + (mouse_coord[0]-mouse_prev_coord[0])*0.3
+		pitch = temp_pitch + (mouse_coord[1]-mouse_prev_coord[1])*0.3
+
+	#print(yaw)
 
 def create_shader(shader_type, source):
     shader = glCreateShader(shader_type)
@@ -70,6 +175,7 @@ class AppOgl(OpenGLFrame):
 		varying vec2 uv0;
 		varying vec3 normal;
 		varying vec4 lamp;
+		
 
 		out vec4 color;
 
@@ -77,6 +183,7 @@ class AppOgl(OpenGLFrame):
 		uniform sampler2D texture1;
 		uniform sampler2D texture2;
 		uniform samplerCube uniform_ReflectionTexture;
+		uniform float time;
 
 		void main()
 		{
@@ -91,15 +198,16 @@ class AppOgl(OpenGLFrame):
 			vec4 normalMap = texture2D(texture1, uv0.xy);
 			vec4 roughnessMap = texture2D(texture2, uv0.xy);
 
-			float dist = dot(vec3(3,0,3), normalMap.xyz);
+			//float dist = dot(vec3(3,0,3), normalMap.xyz);
 			//dist *= (1-roughnessMap.x);
 
 			//vec3 reflectedDirection = reflect(normalize(ws_coords), normal);
 			//vec4 cubeMap = texture(uniform_ReflectionTexture, reflectedDirection).xyz
 
-			vec3 finalColor = diffuseMap.xyz * ambient.xyz;
+			vec3 finalColor = diffuseMap.xyz;
+			finalColor += clamp(cos(uv0.x*5 + time + normalMap.r),-1,1)*abs(1-roughnessMap.r);
 
-		    color.xyz = ( finalColor * dist );
+		    color.xyz = ( finalColor );
 		    color.a = alpha;
 		}""")
 
@@ -112,14 +220,15 @@ class AppOgl(OpenGLFrame):
 		self.diffuse = loadTexture("textures/logo.png")
 		self.normals = loadTexture("textures/logo.png")
 		self.roughness = loadTexture("textures/logo.png")
+		self.time_location = time_location = glGetUniformLocation(self.program, "time")
 
 		self.start = time.time()
 		self.nframes = 0
 
 	def loadNewTexture(self, path):
 		self.diffuse = loadTexture(path)
-		self.normals = loadTexture(path.replace("upscaled","normals").replace(".png","_normal.png"))
-		self.roughness = loadTexture(path.replace("upscaled","roughness").replace(".png","_rough.png"))
+		self.normals = loadTexture(path.split("/")[-1].replace("upscaled","normals").replace(".png","_normal.png"))
+		self.roughness = loadTexture(path.split("/")[-1].replace("upscaled","roughness").replace(".png","_rough.png"))
 
 		texture0 = glGetUniformLocation(self.program, "texture0")
 		glActiveTexture(GL_TEXTURE0)
@@ -145,11 +254,13 @@ class AppOgl(OpenGLFrame):
 		gluPerspective(70, self.width/self.height, 0.1, 200)
 
 		
-		glTranslatef(0, 0, -1.6)
+		glTranslatef(0, 0, -2.8)
 
 		if self.program != -1:
 			glUseProgram(self.program)
-		#glRotatef(self.nframes*0.5, 0, 1, 0)
+
+		glRotatef(yaw, 0, 1, 0)
+		glRotatef(pitch, 1, 0, 0)
 
 		glBegin(GL_QUADS)
 		glColor3f(1, 1, 1)
@@ -170,24 +281,70 @@ class AppOgl(OpenGLFrame):
 
 		tm = time.time() - self.start
 		self.nframes += 1
-		print("fps",self.nframes / tm, end="\r" )
+		glUniform1f(self.time_location, self.nframes*0.01)
 
+		#print("fps",self.nframes / tm, end="\r" )
 
 if __name__ == '__main__':
-	Upscaled = os.listdir("textures/processing/upscaled/")
-
 	root = tkinter.Tk()
 	root.title('OctoTex')
 	root.geometry('1280x720')
 	root.rowconfigure(0, weight=1)
 	app = AppOgl(root, width=320, height=200)
 
+
+	menubar = Menu(root)
+	root.config(menu=menubar)
+	file_menu = Menu(menubar)
+	file_menu.add_command(
+	    label='Load',
+	    command=loadTexturesTkinter
+	)
+
+	file_menu.add_command(
+	    label='Write',
+	    command=writeTexturesBack
+	)
+
+	menubar.add_cascade(
+	    label="Data",
+	    menu=file_menu
+	)
+
+	root.config(menu=menubar)
+	file_menu = Menu(menubar)
+
+	file_menu.add_command(
+	    label='Generate PBR',
+	    command=materializeTexturesTkinter
+	)
+
+	file_menu.add_command(
+	    label='Upscale',
+	    command=upscaleTexturesTkinter
+	)
+
+	file_menu.add_command(
+	    label='Upscale 2x times',
+	    command=upscale2xTexturesTkinter
+	)
+
+	menubar.add_cascade(
+	    label="Textures",
+	    menu=file_menu
+	)
+
+
+	root.bind('<Motion>', motion)
+	root.bind("<ButtonPress-1>", OnMouseDown)
+	root.bind("<ButtonRelease-1>", OnMouseUp)
+
 	def item_selected(event):
 		for selected_item in tree.selection():
 			item = tree.item(selected_item)
 			record = item['values']
 
-			app.loadNewTexture(f"textures/processing/upscaled/{record[0]}")
+			app.loadNewTexture(record[1])
 
 
 
@@ -199,12 +356,32 @@ if __name__ == '__main__':
 	tree = ttk.Treeview(upscaledFrame, columns=columns, show='headings')
 	tree.heading('name', text='textures')
 
+	def updateTextureTree():
+		Upscaled = os.listdir("textures/processing/upscaled/")
+		Diffuse = os.listdir("textures/processing/diffuse/")
 
-	# add data to the treeview
-	for x in range(len(Upscaled)):
-		temp = Upscaled[x]
-		#icon = PhotoImage(file=f"textures/processing/upscaled/{temp}")
-		tree.insert('', tkinter.END, values=(temp))
+		
+		alreadyWas = []
+		#tree.delete()
+
+		for item in tree.get_children():
+			tree.delete(item)
+
+		# add data to the treeview
+		for x in range(len(Upscaled)):
+			temp = Upscaled[x]
+			#icon = PhotoImage(file=f"textures/processing/upscaled/{temp}")
+			tree.insert('', tkinter.END, values=(temp,f"textures/processing/upscaled/{temp}"))
+			alreadyWas.append(temp)
+
+		for x in range(len(Diffuse)):
+			temp = Diffuse[x]
+			if( temp in alreadyWas ):
+				continue
+			#icon = PhotoImage(file=f"textures/processing/upscaled/{temp}")
+			tree.insert('', tkinter.END, values=(temp,f"textures/processing/diffuse/{temp}"))
+
+	updateTextureTree()
 
 	tree.bind('<<TreeviewSelect>>', item_selected)
 	tree.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True)  # Expand and fill in both directions
@@ -213,7 +390,6 @@ if __name__ == '__main__':
 	tree.configure(yscroll=scrollbar.set)
 	scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)  # Place Scrollbar to the right
 
-	
 
 	app.pack(fill=tkinter.BOTH, expand=tkinter.YES)
 	app.animate = 1
